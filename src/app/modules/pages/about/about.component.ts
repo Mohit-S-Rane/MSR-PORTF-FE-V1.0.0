bcf941087ff7c715a95064ef48ecfb12cf11a780
MSRane <mohitsrane16@gmail.com>
2023-01-24 00:45:09 +0530

    jquey added for tags

--- src/app/modules/pages/about/about.component.ts
+++ src/app/modules/pages/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import * as $ from 'jquery';
 
 @Component({
   selector: "about",
@@ -7,5 +8,37 @@ import { Component, OnInit, ViewEncapsulation } from "@angular/core";
   encapsulation: ViewEncapsulation.None,
 })
 export class AboutComponent {
-  constructor() {}
+  constructor() {
+
+    $(document).ready(function () {
+      if (
+        !$("#myCanvas").tagcanvas(
+          {
+            textColour: "#ffffff",
+            outlineColour: "#FE0853",
+            outlineThickness: 0.5,
+            // outlineColour: "#FE0853",
+            maxSpeed: 0.06,
+            freezeActive: true,
+            shuffleTags: true,
+            shape: "sphere",
+            zoom: 0.8,
+            noSelect: true,
+            textFont: null,
+            textHeight: 20,
+            pinchZoom: true,
+            freezeDecel: true,
+            fadeIn: 3000,
+            initial: [0.3, -0.1],
+            depth: 1.1
+          },
+          "tags"
+        )
+      ) {
+        // something went wrong, hide the canvas container
+        $("#myCanvasContainer").hide();
+      }
+    });
+    
+  }
 }
