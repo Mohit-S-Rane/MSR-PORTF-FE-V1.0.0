f570715b24cf237341ceab31302d42affd5d104c
Mohit-S-Rane <mohitsrane16@gmail.com>
2023-01-24 16:07:42 +0530

    Tag Cloud added in About Page

--- src/app/modules/pages/about/about.component.ts
+++ src/app/modules/pages/about/about.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, ViewEncapsulation } from "@angular/core";
-import * as $ from 'jquery';
+import { Component, HostListener, OnInit, SimpleChanges, ViewEncapsulation } from "@angular/core";
+
+declare let TagCloud:any;
 
 @Component({
   selector: "about",
@@ -7,38 +8,30 @@ import * as $ from 'jquery';
   styleUrls: ["./about.component.scss"],
   encapsulation: ViewEncapsulation.None,
 })
-export class AboutComponent {
-  constructor() {
+export class AboutComponent implements OnInit {
+  screenHeight: number;
+  screenWidth: number;
 
-    $(document).ready(function () {
-      if (
-        !$("#myCanvas").tagcanvas(
-          {
-            textColour: "#ffffff",
-            outlineColour: "#FE0853",
-            outlineThickness: 0.5,
-            // outlineColour: "#FE0853",
-            maxSpeed: 0.06,
-            freezeActive: true,
-            shuffleTags: true,
-            shape: "sphere",
-            zoom: 0.8,
-            noSelect: true,
-            textFont: null,
-            textHeight: 20,
-            pinchZoom: true,
-            freezeDecel: true,
-            fadeIn: 3000,
-            initial: [0.3, -0.1],
-            depth: 1.1
-          },
-          "tags"
-        )
-      ) {
-        // something went wrong, hide the canvas container
-        $("#myCanvasContainer").hide();
-      }
-    });
-    
+  myTags = [
+    'JavaScript', 'CSS', 'HTML',
+    'Angular', 'React', 'Node.js',
+    'Devops', 'Jenkins', 'Git',
+    'Docker', 'Node.js', 'Kubernetes',
+    'AWS', 'SAP ABAP', 'jQuery',
+];
+  constructor() {}
+
+  ngOnInit(): void {
+    this.configureTagCloud();
+  }
+
+  configureTagCloud(sWidth = 0) {
+      var tagCloud = TagCloud('.content', this.myTags,{
+        radius: 250,
+        maxSpeed: 'fast',
+        initSpeed: 'fast',
+        direction: 135,
+        keep: true
+      });
   }
 }
